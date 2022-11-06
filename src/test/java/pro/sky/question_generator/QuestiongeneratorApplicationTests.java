99b670bc1cd6f3dfc8b71fd3f2f95e8f2b884122
Denis <demaz@bk.ru>
2022-11-06 17:20:50 +0700

    initial commit

---
+++ src/test/java/pro/sky/question_generator/QuestiongeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.question_generator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestiongeneratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
